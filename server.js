70ff055a5efff166cb44261566ceb0250114e1e2
ArcenIce <95444080+ArcenIce@users.noreply.github.com>
2021-12-02 20:55:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  res.send('Voilà cheh ça a changé')
+  res.send('Hello World! \nVoilà cheh ça a changé ')
 })
 
 app.listen(port, () => {
