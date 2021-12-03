2ecdb539679c19387a5208c3c4140699c48b4558
fabuss254 <fabuss254@outlook.fr>
2021-12-03 01:34:55 +0100

    Acceuil update

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,5 +1,15 @@
 <div>
-    <div class="home-slider" style="background-image: url('./assets/img/thumbnail1.jpg');">
+    <div class="home-slider" style="background-image: url('./assets/img/thumbnail2.jpg');">
         <!--<img src="./assets/img/thumbnail1.jpg" alt="Thumbnail"/> -->
+    </div>
+
+    <div class="text1">
+        <div>
+            <h1>Des histoires...</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida condimentum odio. Proin efficitur diam at dui facilisis, sed sodales sem pharetra. Aliquam justo dolor, auctor sed lacinia eget, dignissim at ipsum. Donec sagittis sit amet turpis in suscipit. Duis pulvinar nisl vitae posuere tincidunt. Nam ultricies tempus lacus, in commodo odio pellentesque eu. Sed vel gravida mauris. Ut dignissim accumsan velit, et rutrum eros vehicula id. Sed facilisis iaculis quam. Nunc nec nisi eu ante pretium tempus. Proin egestas mi ut leo euismod, at aliquam massa aliquet. Donec mollis nunc nec dui elementum condimentum. In efficitur mauris vitae pretium volutpat. Vivamus sed tortor odio. Vestibulum metus ligula, elementum in ullamcorper ut, vehicula at diam. Nunc gravida tellus id lacus dignissim, a elementum sapien blandit.</p>
+        </div>
+        <div>
+            <img src="./assets/img/asset1.png" alt="Logo SNSM" />
+        </div>
     </div>
 </div>
--- src/views/template.php
+++ src/views/template.php
@@ -2,8 +2,16 @@
 <html lang='fr'>
     <head>
         <title><?=$title ?? "\$title non detecté"?></title>
-        <link href="./assets/css/default.css" rel="stylesheet" type="text/css" />
 
+        <!-- FONTS -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Patua+One&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
+
+        <!-- CSS -->
+        <link href="./assets/css/default.css" rel="stylesheet" type="text/css" />
         <link href="./assets/css/home.css" rel="stylesheet" type="text/css" />
 
         <script src="./assets/js/default.js"></script>
